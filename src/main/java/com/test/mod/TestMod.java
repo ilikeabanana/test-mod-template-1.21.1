431a9f70646cbaf2302cac8e623911a43ad5e3d3
Banana Studio <michiel1.debacker@gmail.com>
2024-09-28 15:54:04 +0200

    Initial commit

---
+++ src/main/java/com/test/mod/TestMod.java
@@ -0,0 +1,16 @@
+package com.test.mod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestMod implements ModInitializer {
+	public static final String MOD_ID = "test-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
